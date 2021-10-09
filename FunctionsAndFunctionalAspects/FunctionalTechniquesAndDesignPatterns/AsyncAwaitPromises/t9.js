5d07f31677b6559ccf0c64c05a6af8b11a4ff367
David Ioannis Gugea <davidgugea@yahoo.com>
2021-10-10 00:02:03 +0200

    async/await promises tutorials

--- FunctionsAndFunctionalAspects/FunctionalTechniquesAndDesignPatterns/AsyncAwaitPromises/t9.js
+++ FunctionsAndFunctionalAspects/FunctionalTechniquesAndDesignPatterns/AsyncAwaitPromises/t9.js
@@ -5,3 +5,28 @@ let stocks = {
     toppings : ["chocolate", "peanuts"]
 };
 
+let is_shop_open = true;
+
+let order = (time, work) => {
+    return new Promise(
+        (resolve, reject) => {
+            if(is_shop_open){
+                setTimeout(
+                    () => {
+                        resolve(work);
+                    },
+                    time
+                )
+            }else{
+                reject();
+            }
+        }
+    )
+}
+
+order(
+    2000,
+    () => {
+        console.log(``)
+    }
+);
