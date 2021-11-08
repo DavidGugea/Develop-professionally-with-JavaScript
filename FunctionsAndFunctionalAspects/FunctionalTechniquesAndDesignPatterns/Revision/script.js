91474c52d9599de94a63d2a38464b4fd149925dc
David Ioannis Gugea <davidgugea@yahoo.com>
2021-11-08 22:57:41 +0100

    Trying to understand the event loop

--- FunctionsAndFunctionalAspects/FunctionalTechniquesAndDesignPatterns/Revision/script.js
+++ FunctionsAndFunctionalAspects/FunctionalTechniquesAndDesignPatterns/Revision/script.js
@@ -1,10 +1,35 @@
-function printOneThenPrintTwo(){
-    console.log(1);
-
-    printOneThenPrintTwo = function(){
-        console.log(2);
-    }
+const test1 = async () => {
+    setTimeout(
+        () => {
+            return 5;
+        },
+        2000
+    )
 }
+const test2 = () => new Promise(
+    (resolve, _) => {
+        setTimeout(
+            () => resolve(25),
+            1000
+        )
+    }
+);
+const test3 = () => new Promise(
+    (resolve, _) => {
+        setTimeout(
+            () => {
+                console.log('second');
+                resolve();
+            },
+            2000
+        )
+    }
+);
 
-printOneThenPrintTwo();
-printOneThenPrintTwo();
+(
+    async () => {
+        console.log("first");
+        await test3();
+        console.log("third");
+    }
+)();
